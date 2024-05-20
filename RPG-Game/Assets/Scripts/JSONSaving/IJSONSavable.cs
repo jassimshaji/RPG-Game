70d3bb85d6a7c64c4a253bf79b016adfca556038
jassimshaji <66567742+jassimshaji@users.noreply.github.com>
2024-05-20 20:11:23 +0530

    JSONSavingSystem
    implementing IJSONSavable.cs file in new branch

---
+++ RPG-Game/Assets/Scripts/JSONSaving/IJSONSavable.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace RPG.JSONSaving
+{
+    public interface IJSONSavable
+    {
+            /// <summary>
+            /// Override to return a JToken representing the state of the IJsonSaveable
+            /// </summary>
+            /// <returns>A JToken</returns>
+            JToken CaptureAsJToken();
+            /// <summary>
+            /// Restore the state of the component using the information in JToken.
+            /// </summary>
+            /// <param name="state">A JToken object representing the state of the module</param>
+            void RestoreFromJToken(JToken state);
+        }
+    }
+
+
