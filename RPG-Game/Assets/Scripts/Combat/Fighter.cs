4f69b7e39206c2f4e27e9e103e290699ff6032e0
jassimshaji <66567742+jassimshaji@users.noreply.github.com>
2024-05-26 14:26:03 +0530

    added scriptable Object
    new scriptable object and sword

--- RPG-Game/Assets/Scripts/Combat/Fighter.cs
+++ RPG-Game/Assets/Scripts/Combat/Fighter.cs
@@ -1,33 +1,44 @@
 using RPG.Movement;
 using RPG.Core;
 using UnityEngine;
+using System;
 
 
 
 
 namespace RPG.Combat
 {
-    
+
     public class Fighter : MonoBehaviour, IAction
 
     {
-        
+
         [SerializeField] float WeaponRange = 2f;
         [SerializeField] float TimeBetweenAttacks = 1f;
         [SerializeField] float Damage = 5f;
+        [SerializeField] GameObject weaponPrefab = null;
+        [SerializeField] Transform handTransform = null;
+        [SerializeField] AnimatorOverrideController weaponOverride = null;
         Health target;
         float TimeSinceLastAttack = Mathf.Infinity;
-        
+
         Mover Move;
         private void Start()
         {
+            
             Move = GetComponent<Mover>();
+            if(weaponPrefab == null || handTransform == null ) return;
+            SpawnWeapon();
+            Animator animator = GetComponent<Animator>();
+            animator.runtimeAnimatorController = weaponOverride;
         }
+
+
         void Update()
         {
             TimeSinceLastAttack += Time.deltaTime;
             if (target == null) return;
-            if(target.IsDead()) return;
+            if (target.IsDead()) return;
             if (!GetInRange())
             {
                 Move.MoveTo(target.transform.position, 1f);
@@ -38,16 +49,19 @@ namespace RPG.Combat
                 AttackBehaviour();
             }
 
+        }
 
 
-
+        private void SpawnWeapon()
+        {
+            Instantiate(weaponPrefab, handTransform);
         }
 
         public bool CanAttack(GameObject combatTarget)
         {
             if (combatTarget == null) return false;
-            Health targetToTest =combatTarget.GetComponent<Health>();
-            return combatTarget != null && !targetToTest.IsDead(); 
+            Health targetToTest = combatTarget.GetComponent<Health>();
+            return combatTarget != null && !targetToTest.IsDead();
 
         }
         public void Attack(GameObject combatTarget)
@@ -60,7 +74,7 @@ namespace RPG.Combat
         private void AttackBehaviour()
         {
             transform.LookAt(target.transform);
-            if (TimeSinceLastAttack>TimeBetweenAttacks)
+            if (TimeSinceLastAttack > TimeBetweenAttacks)
             {
                 //Here Hit() will get triggered
                 AttackTrigger();
@@ -77,7 +91,7 @@ namespace RPG.Combat
 
         void Hit()
         {
-            if(target == null) return;
+            if (target == null) return;
             target.TakeDamage(Damage);
         }
 
@@ -85,6 +99,8 @@ namespace RPG.Combat
         {
             return Vector3.Distance(transform.position, target.transform.position) < WeaponRange;
         }
+
+
 
 
 
---
+++ RPG-Game/Assets/Scripts/Combat/Weapon.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace RPG.Combat
+{
+    [CreateAssetMenu(fileName = "Weapon", menuName = "Weapons/Make New Weapon", order = 0)]
+    public class Weapon : ScriptableObject
+    {
+
+    }
+    
+}
