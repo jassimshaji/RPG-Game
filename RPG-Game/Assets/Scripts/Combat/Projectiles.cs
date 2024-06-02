c5fd26f84b5c77975e16003d1105a5a2ca9f7d88
jassimshaji <66567742+jassimshaji@users.noreply.github.com>
2024-06-02 12:29:48 +0530

    particle system
    added fireball particle system

--- RPG-Game/Assets/Scripts/Combat/Projectiles.cs
+++ RPG-Game/Assets/Scripts/Combat/Projectiles.cs
@@ -8,11 +8,19 @@ namespace RPG.Combat
     {
         Health target = null;
         [SerializeField] float projectileSpeed = 3f;
+        [SerializeField] bool isHoming = true;
         float damage = 0;
+
+        private void Start() {
+            transform.LookAt(GetAimLocation());
+        }
         private void Update() {
 
-            if(target == null) return;
+            if(target == null) return; 
+
+            if(isHoming && !target.IsDead()){
             transform.LookAt(GetAimLocation());
+            }
             transform.Translate(Vector3.forward * projectileSpeed * Time.deltaTime);
 
             
@@ -29,13 +37,17 @@ namespace RPG.Combat
         {
             CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
             if(targetCapsule == null){
-                return target.transform.position;
+                return target.transform.position;   
             }
-            return target.transform.position + Vector3.up * targetCapsule.height / 2; 
+  
+            
+            return target.transform.position + Vector3.up * targetCapsule.height / 2;
+           
         }
 
         private void OnTriggerEnter(Collider other) {
             if(other.GetComponent<Health>() != target) return;
+            if(target.IsDead())return;
             target.TakeDamage(damage);
             Destroy(gameObject);
         }
